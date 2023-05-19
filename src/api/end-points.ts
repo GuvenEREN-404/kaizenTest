290ca115af9f37fe1bfe254b6523733fa5c25980
guven <guven.eren@bulutmd.com>
2023-05-19 19:56:34 +0300

    api folder added

---
+++ src/api/end-points.ts
@@ -0,0 +1,12 @@
+const MAIN_URL = 'https://api.extrazone.com'
+
+export const END_POINTS = {
+    TAGS:{
+        GET_TAGS:`${MAIN_URL}/tags/list`,
+    },
+    PROMOTIONS:{
+        PROMOTIONS_LIST:`${MAIN_URL}/promotions/list?Channel=PWA`,
+        PROMOTIONS_BY_ID:`${MAIN_URL}promotions?Id={Id}`,
+    }
+   
+}
